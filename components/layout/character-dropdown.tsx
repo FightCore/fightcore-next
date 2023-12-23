c0ea19486cb903024329227f8989aaea58c1aa36
Bart de Bever <bartdebever112@gmail.com>
2023-12-23 20:47:02 +0100

    Convert to single package

---
+++ components/layout/character-dropdown.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React from 'react';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+  Button,
+} from '@nextui-org/react';
+
+export const CharacterMenu = () => {
+  'use client';
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button variant='bordered'>Open Menu</Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label='Static Actions'>
+        <DropdownItem key='new'>New file</DropdownItem>
+        <DropdownItem key='copy'>Copy link</DropdownItem>
+        <DropdownItem key='edit'>Edit file</DropdownItem>
+        <DropdownItem key='delete' className='text-danger' color='danger'>
+          Delete file
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
