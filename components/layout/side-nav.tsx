c276a655fd3c8f78e15e4760b40f7da628cfcbb4
Bart de Bever <bartdebever112@gmail.com>
2024-01-02 20:39:28 +0100

    Sidenav sections

--- components/layout/side-nav.tsx
+++ components/layout/side-nav.tsx
@@ -1,38 +1,96 @@
 'use client';
-import { Listbox, ListboxItem, ListboxSection } from '@nextui-org/react';
+import { Divider, Image, Link, Tooltip } from '@nextui-org/react';
 import { Logo } from '../icons';
-import { ListboxWrapper } from './listbox-wrapper';
+import { characters } from '@/config/characters';
+import React from 'react';
 
 export const SideNav = () => {
   'use client';
   return (
-    <>
+    <div className='p-2'>
       <div className='w-100 align-content-center flex p-3'>
         <Logo className='flex-1' height={50} width={100} />
       </div>
-      <ListboxWrapper>
-        <Listbox variant='flat' aria-label='Listbox menu with sections'>
-          <ListboxSection title='Actions' showDivider>
-            <ListboxItem key='new'>New file</ListboxItem>
-            <ListboxItem key='copy' description='Copy the file link'>
-              Copy link
-            </ListboxItem>
-            <ListboxItem key='edit' description='Allows you to edit the file'>
-              Edit file
-            </ListboxItem>
-          </ListboxSection>
-          <ListboxSection title='Danger zone'>
-            <ListboxItem
-              key='delete'
-              className='text-danger'
-              color='danger'
-              description='Permanently delete the file'
+
+      <div className='p-2 grid grid-cols-4 gap-2'>
+        {characters.map((character) => (
+          <div key={character.normalizedName}>
+            <Tooltip content={character.name} delay={1000}>
+              <Image
+                className='grow'
+                alt={character.name}
+                width={40}
+                src={
+                  'https://i.fightcore.gg/melee/stocks/' +
+                  character.name +
+                  '.png'
+                }
+              />
+            </Tooltip>
+          </div>
+        ))}
+      </div>
+      <Divider className='my-4' />
+
+      <ul className='space-y-2 font-medium'>
+        <li>
+          <Link
+            color='foreground'
+            href='#'
+            className='flex items-center p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group'
+          >
+            <svg
+              className='w-5 h-5 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              viewBox='0 0 22 21'
             >
-              Delete file
-            </ListboxItem>
-          </ListboxSection>
-        </Listbox>
-      </ListboxWrapper>
-    </>
+              <path d='M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z' />
+              <path d='M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z' />
+            </svg>
+            <span className='ms-3'>Characters</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            color='foreground'
+            href='#'
+            className='flex items-center p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group'
+          >
+            <svg
+              className='w-5 h-5 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              viewBox='0 0 22 21'
+            >
+              <path d='M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z' />
+              <path d='M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z' />
+            </svg>
+            <span className='ms-3'>Moves</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            color='foreground'
+            href='#'
+            className='flex items-center p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group'
+          >
+            <svg
+              className='w-5 h-5 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              viewBox='0 0 22 21'
+            >
+              <path d='M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z' />
+              <path d='M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z' />
+            </svg>
+            <span className='ms-3'>Tournaments</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
---
+++ config/characters.ts
@@ -0,0 +1,106 @@
+export const characters = [
+  {
+    name: 'Bowser',
+    normalizedName: 'bowser',
+  },
+  {
+    name: 'Captain Falcon',
+    normalizedName: 'captainfalcon',
+  },
+  {
+    name: 'Donkey Kong',
+    normalizedName: 'donkeykong',
+  },
+  {
+    name: 'Dr. Mario',
+    normalizedName: 'drmario',
+  },
+  {
+    name: 'Falco',
+    normalizedName: 'falco',
+  },
+  {
+    name: 'Fox',
+    normalizedName: 'fox',
+  },
+  {
+    name: 'Ganondorf',
+    normalizedName: 'ganondorf',
+  },
+  {
+    name: 'Ice Climbers',
+    normalizedName: 'iceclimbers',
+  },
+  {
+    name: 'Jigglypuff',
+    normalizedName: 'jigglypuff',
+  },
+  {
+    name: 'Kirby',
+    normalizedName: 'kirby',
+  },
+  {
+    name: 'Link',
+    normalizedName: 'link',
+  },
+  {
+    name: 'Luigi',
+    normalizedName: 'luigi',
+  },
+  {
+    name: 'Mario',
+    normalizedName: 'mario',
+  },
+  {
+    name: 'Marth',
+    normalizedName: 'marth',
+  },
+  {
+    name: 'Mewtwo',
+    normalizedName: 'mewtwo',
+  },
+  {
+    name: 'Mr. Game & Watch',
+    normalizedName: 'mrgameandwatch',
+  },
+  {
+    name: 'Ness',
+    normalizedName: 'ness',
+  },
+  {
+    name: 'Pichu',
+    normalizedName: 'pichu',
+  },
+  {
+    name: 'Pikachu',
+    normalizedName: 'pikachu',
+  },
+  {
+    name: 'Princess Peach',
+    normalizedName: 'peach',
+  },
+  {
+    name: 'Princess Zelda',
+    normalizedName: 'zelda',
+  },
+  {
+    name: 'Roy',
+    normalizedName: 'roy',
+  },
+  {
+    name: 'Samus',
+    normalizedName: 'samus',
+  },
+  {
+    name: 'Sheik',
+    normalizedName: 'sheik',
+  },
+  {
+    name: 'Yoshi',
+    normalizedName: 'yoshi',
+  },
+  {
+    name: 'Young Link',
+    normalizedName: 'younglink',
+  },
+];
