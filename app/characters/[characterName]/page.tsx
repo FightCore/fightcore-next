20031fcce70b28b813c0d43fbb923569a17a8fe3
Bart de Bever <bartdebever112@gmail.com>
2024-01-11 16:54:09 +0100

    preload the first 5 moves

--- app/characters/[characterName]/page.tsx
+++ app/characters/[characterName]/page.tsx
@@ -1,6 +1,7 @@
 import { MoveCard } from '@/components/moves/move-card';
 import { characters } from '@/config/framedata/framedata';
 import { promises as fs } from 'fs';
+import Head from 'next/head';
 
 export function generateStaticParams() {
   return characters.map((character) => {
@@ -21,6 +22,22 @@ export default async function Character({
   const character = JSON.parse(file);
   return (
     <>
+      <Head>
+        {character.moves.slice(5).map((move: any) => (
+          <link
+            key={move.normalizedName}
+            rel='preload'
+            as='image'
+            href={
+              'https://i.fightcore.gg/melee/moves/' +
+              params.characterName +
+              '/' +
+              move.normalizedName +
+              '.webm'
+            }
+          />
+        ))}
+      </Head>
       <h1>{character.name}</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
         {character.moves.map((move: any, index: number) => (
