41d0a6d75b97cc3371822634b0b974bd3b3b17f9
Bart de Bever <bartdebever112@gmail.com>
2024-08-02 23:56:22 +0200

    Rework the crouch cancel move calculator to work again

--- website/components/moves/crouch-cancel/crouch-cancel-move-overview-table.tsx
+++ website/components/moves/crouch-cancel/crouch-cancel-move-overview-table.tsx
@@ -1,9 +1,11 @@
 import { CharacterBase } from "@/models/character";
 import { Move } from "@/models/move";
 import { calculateCrouchCancelPercentage } from "@/utilities/crouch-cancel-calculator";
+import { processDuplicateHitboxes, processDuplicateHits } from "@/utilities/hitbox-utils";
 import { moveRoute } from "@/utilities/routes";
 import { Link } from "@nextui-org/link";
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/table";
+import { flattenData, FlattenedHitbox } from "../hitboxes/hitbox-table-columns";
 
 export interface CrouchCancelMoveOverviewTableParams {
   character: CharacterBase;
@@ -13,47 +15,65 @@ export interface CrouchCancelMoveOverviewTableParams {
   floorPercentage: boolean;
 }
 
-export function CrouchCancelMoveOverviewTable(data: Readonly<CrouchCancelMoveOverviewTableParams>) {
+interface ExpandedFlattenedHitbox extends FlattenedHitbox {
+  move: Move;
+}
+
+export function CrouchCancelMoveOverviewTable(params: Readonly<CrouchCancelMoveOverviewTableParams>) {
   const classNames = {
     wrapper: ["dark:bg-gray-800", "border-0", "shadow-none", "p-0"],
     th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],
     td: ["text-default-600", "py-1", "group-data-[odd=true]:before:dark:bg-gray-700"],
   };
 
+  const flattenedHits = params.moves.flatMap((move) => {
+    if (!move.hits || move.hits.length === 0) {
+      return [];
+    }
+
+    const processedHits = processDuplicateHitboxes(move.hits!);
+    const data = processDuplicateHits(flattenData(processedHits));
+    return data.map((hitbox) => ({
+      ...hitbox,
+      move: move,
+    }));
+  });
+
+  let previousName = "";
+
   return (
-    <h2>This component has been temporarily disabled due to the hitbox rework. Please visit the main website.</h2>
-    // <Table classNames={classNames} isStriped>
-    //   <TableHeader>
-    //     <TableColumn>Name</TableColumn>
-    //     <TableColumn>Hitbox</TableColumn>
-    //     <TableColumn>Breaks at percentage</TableColumn>
-    //   </TableHeader>
-    //   <TableBody>
-    //     {data.moves
-    //       .filter((move) => move.hits && move.hits.length > 0)
-    //       .map((move) => {
-    //         return (
-    //           <TableRow key={move.id.toString()}>
-    //             <TableCell>
-    //               <Link href={moveRoute(data.character, move)}>{move.name}</Link>
-    //             </TableCell>
-    //             <TableCell>
-    //               {move.hitboxes?.map((hitbox) => (
-    //                 <div key={hitbox.id.toString() + "name"}>{hitbox.name}</div>
-    //               ))}
-    //             </TableCell>
-    //             <TableCell>
-    //               {move.hitboxes?.map((hitbox) => (
-    //                 <div key={hitbox.id + "percentage"}>
-    // eslint-disable-next-line max-len
-    //                   {calculateCrouchCancelPercentage(hitbox, data.target, data.knockbackTarget, data.floorPercentage)}
-    //                 </div>
-    //               ))}
-    //             </TableCell>
-    //           </TableRow>
-    //         );
-    //       })}
-    //   </TableBody>
-    // </Table>
+    <Table classNames={classNames} isStriped>
+      <TableHeader>
+        <TableColumn>Name</TableColumn>
+        <TableColumn>Hit</TableColumn>
+        <TableColumn>Hitbox</TableColumn>
+        <TableColumn>Breaks at percentage</TableColumn>
+      </TableHeader>
+      <TableBody>
+        {flattenedHits.map((hitbox) => {
+          const html = (
+            <TableRow key={hitbox.id.toString()}>
+              <TableCell>
+                {previousName === hitbox.move.name ? (
+                  ""
+                ) : (
+                  <Link href={moveRoute(params.character, hitbox.move)}>{hitbox.move.name}</Link>
+                )}
+              </TableCell>
+              <TableCell width={100} className="md:text-center">
+                {hitbox.hit}
+              </TableCell>
+              <TableCell>{hitbox.name}</TableCell>
+              <TableCell className="md:text-center">
+                {calculateCrouchCancelPercentage(hitbox, params.target, params.knockbackTarget, params.floorPercentage)}
+              </TableCell>
+            </TableRow>
+          );
+
+          previousName = hitbox.move.name;
+          return html;
+        })}
+      </TableBody>
+    </Table>
   );
 }
