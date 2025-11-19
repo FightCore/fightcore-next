1f7cede99e32c8a0e5b1e62ac3e18d20276b4854
Bart de Bever <bartdebever112@gmail.com>
2025-11-19 12:25:17 +0100

    fix: Fix the markup of the credits page and updated some links

--- website/pages/credits.tsx
+++ website/pages/credits.tsx
@@ -1,5 +1,5 @@
 import { PageTitle } from '@/components/page-title';
-import { Link } from "@heroui/link";
+import { Link } from '@heroui/link';
 
 export default function Credits() {
   return (
@@ -7,17 +7,17 @@ export default function Credits() {
       <PageTitle title="Credits & Sources" />
 
       <section className="mb-5">
-        <h2 className="text-bold mb-3 mt-3 text-xl">Credits</h2>
+        <h2 className="text-bold mt-3 mb-3 text-xl">Credits</h2>
         <p>A more detailed list of people that have been vital to this project.</p>
         <ul className="ml-7 list-disc">
           <li>
-            <strong>Bort,</strong> main developer of the website.
+            <strong>Bort</strong>, main developer of the website.
           </li>
           <li>
-            <Link href="https://x.com/hrtfiend" isExternal>
-              Emilia,
-            </Link>{' '}
-            recorder of all the GIFs and maintainer of{' '}
+            <Link href="https://bsky.app/profile/hrtfiend.bsky.social" isExternal>
+              Emilia
+            </Link>
+            , recorder of all the GIFs and maintainer of{' '}
             <Link href="https://drive.fightcore.gg" isExternal>
               the Google Drive
             </Link>
@@ -25,13 +25,15 @@ export default function Credits() {
           </li>
           <li>
             <Link href="https://github.com/neilharbin0" isExternal>
-              NeilHarbin0,
-            </Link>{' '}
-            creating frame data software allowing for background-less recording and easy editing.
+              NeilHarbin0
+            </Link>
+            , creating frame data software allowing for background-less recording and easy editing.
           </li>
           <li>
-            <strong>skullbro</strong>, providing insights in how to improve the crouch cancel and ASDI Down
-            calculations.
+            <Link href="https://bsky.app/profile/skullbrossbm.bsky.social" isExternal>
+              skullbro
+            </Link>
+            , providing insights in how to improve the crouch cancel and ASDI Down calculations.
           </li>
           <li>
             <strong>Dutch Melee Community</strong>, for their continued support and feedback.
@@ -41,7 +43,7 @@ export default function Credits() {
       <section className="my-5">
         <h2 className="text-bold mb-3 text-xl">Sources</h2>
         <p>The following is a list of sources used when building FightCore.</p>
-        <ul className="ml-7 mt-3 list-disc">
+        <ul className="mt-3 ml-7 list-disc">
           <li>
             <Link href="http://meleeframedata.com" isExternal>
               MeleeFrameData
@@ -58,7 +60,14 @@ export default function Credits() {
             </Link>
           </li>
           <li>
-            <Link href="https://www.npmjs.com/package/@wizpanda/super-gif">Wizpanda&apos;s fork of SuperGIF</Link>
+            <Link href="https://www.npmjs.com/package/@wizpanda/super-gif" isExternal>
+              Wizpanda&apos;s fork of SuperGIF
+            </Link>
+          </li>
+          <li>
+            <Link href="https://melee-framedata.theshoemaker.de/" isExternal>
+              The Shoemaker&apos;s Melee Frame Data
+            </Link>
           </li>
         </ul>
       </section>
